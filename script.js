98d3b19c33634b3508407e9c485c206b77ca1a16
Zain <zainharoon890@gmail.com>
2021-03-19 15:43:19 +0500

    Navbar Name Changed

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+$(function () {
+    $("#myForm").submit(function(){
+        var password = $("#pass-input")
+        var confirm_pass= $("#confirm_pass-input")
+        var name = $("#name-input")
+        var email = $("#email-input")
+        var Submit_form=true;
+        // if(email.val() =='' || name.val() =='' || password.val() ==''||confirm_pass.val() =='')
+        // {
+        //     alert("Some input field is empty")
+        //     return false;
+        // }
+        if(name.val().length<5 || name.val()=='')
+        {
+            alert("Name must be Greater than 5 characters");
+            name.css("border","2px solid red")
+            $("#name-condition").css({"color":"red","transition":"1s"})
+            Submit_form=false;
+            // return false;
+        }
+        else{
+            name.css("border","none")
+            $("#name-condition").css({"color":"white"})
+            
+        }
+        if(email.val()=='')
+        {
+            $("#email-input").css("border","2px solid red")
+            alert("Email Input Field is Empty")
+            $("#email-condition").css({"color":"red","transition":"0.8s"})
+            Submit_form=false;
+        }
+        else{
+            $("#email-input").css("border","none")
+            $("#email-condition").css({"color":"white"})
+        }
+        if(password.val().length<8 || password.val()=='')
+        {
+            alert("Password must be greater than 8 characters")
+            password.css("border","2px solid red")
+            $("#pass-condition").css({"color":"red","transition":"1s"})
+            Submit_form=false;
+            // return false;
+        }
+        else{
+            password.css("border","none")
+            $("#pass-condition").css({"color":"white"})
+        }
+        if(password.val()!=confirm_pass.val())
+        {
+            alert("Confirm Password does not match");
+            confirm_pass.css("border","2px solid red")
+            $("#conf-pass").css({"color":"red","transition":"1s"})
+            Submit_form=false;
+            // return false;
+        }
+        else{
+            confirm_pass.css("border","none")
+            $("#conf-pass").css({"color":"white"})
+        }
+        if(Submit_form==true)
+        {
+            alert("FORM SUBMITTED SUCCESSFULLY")
+            this.preventDefault()
+            return true;
+        }
+        else
+        {
+            $("#form-condition").css({"color":"red","transition":"1s"})         
+            // this.preventDefault()
+            return false;
+        }
+    });
+});
 // $(function () {
 //     var  name = document.getElementById('name-input');
 //     var password= document.getElementById('pass-input')
@@ -78,77 +152,3 @@
 //     }
 //   });
 // });
-$(function () {
-    $("#myForm").submit(function(){
-        var password = $("#pass-input")
-        var confirm_pass= $("#confirm_pass-input")
-        var name = $("#name-input")
-        var email = $("#email-input")
-        var Submit_form=true;
-        // if(email.val() =='' || name.val() =='' || password.val() ==''||confirm_pass.val() =='')
-        // {
-        //     alert("Some input field is empty")
-        //     return false;
-        // }
-        if(name.val().length<5 || name.val()=='')
-        {
-            alert("Name must be Greater than 5 characters");
-            name.css("border","2px solid red")
-            $("#name-condition").css({"color":"red","transition":"1s"})
-            Submit_form=false;
-            // return false;
-        }
-        else{
-            name.css("border","none")
-            $("#name-condition").css({"color":"white"})
-            
-        }
-        if(email.val()=='')
-        {
-            $("#email-input").css("border","2px solid red")
-            alert("Email Input Field is Empty")
-            $("#email-condition").css({"color":"red","transition":"0.8s"})
-            Submit_form=false;
-        }
-        else{
-            $("#email-input").css("border","none")
-            $("#email-condition").css({"color":"white"})
-        }
-        if(password.val().length<8 || password.val()=='')
-        {
-            alert("Password must be greater than 8 characters")
-            password.css("border","2px solid red")
-            $("#pass-condition").css({"color":"red","transition":"1s"})
-            Submit_form=false;
-            // return false;
-        }
-        else{
-            password.css("border","none")
-            $("#pass-condition").css({"color":"white"})
-        }
-        if(password.val()!=confirm_pass.val())
-        {
-            alert("Confirm Password does not match");
-            confirm_pass.css("border","2px solid red")
-            $("#conf-pass").css({"color":"red","transition":"1s"})
-            Submit_form=false;
-            // return false;
-        }
-        else{
-            confirm_pass.css("border","none")
-            $("#conf-pass").css({"color":"white"})
-        }
-        if(Submit_form==true)
-        {
-            alert("FORM SUBMITTED SUCCESSFULLY")
-            this.preventDefault()
-            return true;
-        }
-        else
-        {
-            $("#form-condition").css({"color":"red","transition":"1s"})         
-            // this.preventDefault()
-            return false;
-        }
-    });
-});
